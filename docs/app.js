9efe03fd7677cb54cd05457ce4a6757cc9368823
danikk965 <kekdanik04@gmail.com>
2025-03-08 05:04:38 +0300

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,21 +1,28 @@
-TweenLite.set("#petals", { perspective:600 })
-TweenLite.set("img", { xPercent:"-50%", yPercent:"-50%" })
+TweenLite.set("#petals", { perspective: 600 });
+TweenLite.set("img", { xPercent: "-50%", yPercent: "-50%" });
+
 var total = 50;
 var warp = document.getElementById("petals"),
-w = window.innerWidth,
-h = window.innerHeight;
-for (i = 0; i < total; i++) {
+    w = window.innerWidth,
+    h = window.innerHeight;
+
+for (var i = 0; i < total; i++) {
     var Div = document.createElement('div');
-    TweenLite.set(Div, { attr: { class:'dot' }, x:R(0,w), y:R(-200,-150), z:R(-200,200)});
+    TweenLite.set(Div, { attr: { class: 'dot' }, x: R(0, w), y: R(-200, -150), z: R(-200, 200) });
     warp.appendChild(Div);
     animm(Div);
 }
+
 function animm(elm) {
-    TweenMax.to(elm,R(6,15), { y:h+100, ease:Linear.easeNone, repeat:-1, delay:-15 });
-    TweenMax.to(elm,R(4,8), { x:'+=100', rotationZ:R(0,180), repeat:-1, yoyo:true, ease:Sine.easeInOut });
-    TweenMax.to(elm,R(2,8), { rotationX:R(0,360), rotationY:R(0,360), repeat:-1, yoyo:true, ease:Sine.easeInOut, delay:-5 });
-};
-function R(min,max) { return min+Math.random() * (max-min) };
+    TweenMax.to(elm, R(6, 15), { y: h + 100, ease: Linear.easeNone, repeat: -1, delay: -15 });
+    TweenMax.to(elm, R(4, 8), { x: '+=100', rotationZ: R(0, 180), repeat: -1, yoyo: true, ease: Sine.easeInOut });
+    TweenMax.to(elm, R(2, 8), { rotationX: R(0, 360), rotationY: R(0, 360), repeat: -1, yoyo: true, ease: Sine.easeInOut, delay: -5 });
+}
+
+function R(min, max) {
+    return min + Math.random() * (max - min);
+}
+
 var typed = new Typed('#text', {
     strings: ['Желаю весеннего настроения, пусть на душе всегда будет светло и радостно. Желаю, чтобы на лице всегда сияла улыбка, пусть жизнь будет щедра на подарки. В самый женский день года желаю простого женского счастья и здоровья.'],
     startDelay: 3000,
